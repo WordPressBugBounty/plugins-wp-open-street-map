d61c4b2ff81638e7570f8c56b0922741fe86b5d2
Sean Murphy <sean@iamseanmurphy.com>
2024-10-17 14:35:52 +0000

    1.31

--- wp-open-street-map/views/icons_list.php
+++ wp-open-street-map/views/icons_list.php
@@ -1,10 +1,19 @@
 <ul>
+
 <?php
 
+
+
 	foreach($icons_list as $icon)
+
 	{
-		echo '<li rel="'.$icon.'"><i class="fa fa-'.$icon.'"></i>'.$icon.'</li>';
+
+		echo '<li rel="'.esc_attr($icon).'"><i class="fa fa-'.esc_attr($icon).'"></i>'.esc_attr($icon).'</li>';
+
 	}
 
+
+
 ?>
+
 </ul>
--- wp-open-street-map/views/map.tpl.php
+++ wp-open-street-map/views/map.tpl.php
@@ -1,13 +1,25 @@
-<div id="wp_osm_<?= $map->id ?>" class="wp_osm" style="width: <?= $map->width ?>; height: <?= $map->height ?>" data-lon="<?= $map->longitude ?>" data-lat="<?= $map->latitude ?>" data-zoom="<?= $map->zoom ?>">
+<div id="wp_osm_<?php (int)$map->id ?>" class="wp_osm" style="width: <?php echo esc_attr($map->width) ?>; height: <?php echo esc_attr($map->height) ?>" data-lon="<?php echo (int)$map->longitude ?>" data-lat="<?php echo (int)$map->latitude ?>" data-zoom="<?php echo (int)$map->zoom ?>">
+
 	<div class="ol-popup">
+
 	    <a href="#" class="ol-popup-closer"></a>
+
 	    <div class="popup-content"></div>
+
 	</div>
+
 	<?php
 
+
+
 		foreach($map->markers as $marker)
+
 		{
-			echo '<div class="marker" data-icon="'.$marker->icon.'" data-lon="'.$marker->longitude.'" data-lat="'.$marker->latitude.'" data-name="'.$marker->name.'" data-description="'.nl2br($marker->description).'"></div>';
+
+			echo '<div class="marker" data-icon="'.esc_url($marker->icon).'" data-lon="'.(int)$marker->longitude.'" data-lat="'.(int)$marker->latitude.'" data-name="'.esc_attr($marker->name).'" data-description="'.wp_kses_post(nl2br($marker->description)).'"></div>';
+
 		}
+
 	?>
+
 </div>
